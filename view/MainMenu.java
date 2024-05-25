b59da5e64dba248eae9131b666c44bb877c88a87
thanh1072004 <trancongthanha6@gmail.com>
2024-05-25 13:51:18 +0700

    Update MainMenu

--- view/MainMenu.java
+++ view/MainMenu.java
@@ -1,6 +1,12 @@
 package view;
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+
+import swing.MyTextField;
 
 import java.awt.*;
 import javax.imageio.ImageIO;
@@ -18,7 +24,7 @@ public class MainMenu extends JFrame {
 
         JPanel leftPanel = new JPanel();
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-        leftPanel.setBackground(new Color(0, 102, 0)); // Màu xanh đen
+        leftPanel.setBackground(new Color(255, 223, 128)); 
         leftPanel.setPreferredSize(new Dimension(200, getHeight()));
 
         JLabel usernameLabel = new JLabel("Username");
@@ -27,7 +33,7 @@ public class MainMenu extends JFrame {
         Image scaledImage_user = originalIcon_user.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon_user = new ImageIcon(scaledImage_user);
         usernameLabel.setIcon(scaledIcon_user);
-        usernameLabel.setForeground(Color.WHITE);
+        usernameLabel.setForeground(Color.BLACK);
         usernameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         usernameLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
         leftPanel.add(usernameLabel);
@@ -38,27 +44,27 @@ public class MainMenu extends JFrame {
 
         JMenuBar menuBar = new JMenuBar();
         menuBar.setLayout(new BoxLayout(menuBar, BoxLayout.Y_AXIS));
-        menuBar.setBackground(new Color(0, 102, 0)); // Màu xanh đen
+        menuBar.setBackground(new Color(255, 223, 128)); 
         menuBar.setBorder(BorderFactory.createEmptyBorder(20, 0, 0, 0));
 
-        JMenu eventsMenu = createMenu("Events", Color.WHITE);
+        JMenu eventsMenu = createMenu("Events", Color.BLACK);
         ImageIcon originalIcon = new ImageIcon(Toolkit.getDefaultToolkit().createImage(
                 MainMenu.class.getResource("/icon/event.png")));
         Image scaledImage = originalIcon.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon = new ImageIcon(scaledImage);
         eventsMenu.setIcon(scaledIcon);
-        eventsMenu.add(createMenuItem("Event List", Color.WHITE));
-        eventsMenu.add(createMenuItem("Create Event", Color.WHITE));
+        eventsMenu.add(createMenuItem("View Event", Color.BLACK));
+        eventsMenu.add(createMenuItem("Create Event", Color.BLACK));
         menuBar.add(eventsMenu);
 
-        JMenu invitationsMenu = createMenu("Invitations", Color.WHITE);
+        JMenu invitationsMenu = createMenu("Invitations", Color.BLACK);
         ImageIcon originalIcon_invite = new ImageIcon(Toolkit.getDefaultToolkit().createImage(
                 MainMenu.class.getResource("/icon/list.png")));
         Image scaledImage_invite = originalIcon_invite.getImage().getScaledInstance(16, 16, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon_invite = new ImageIcon(scaledImage_invite);
         invitationsMenu.setIcon(scaledIcon_invite);
-        invitationsMenu.add(createMenuItem("Requests", Color.WHITE));
-        invitationsMenu.add(createMenuItem("Invite User", Color.WHITE));
+        invitationsMenu.add(createMenuItem("Requests", Color.BLACK));
+        invitationsMenu.add(createMenuItem("Invite User", Color.BLACK));
         menuBar.add(invitationsMenu);
 
         leftPanel.add(menuBar);
@@ -72,7 +78,10 @@ public class MainMenu extends JFrame {
 
         JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         searchPanel.setBackground(Color.WHITE);
-        JTextField searchField = new JTextField("Search", 20);
+        MyTextField searchField = new MyTextField();
+        searchField.setHint("Search");
+        searchField.setColumns(20);
+        searchField.setPreferredSize(new Dimension(2000, 30));
         searchPanel.add(searchField);
         topPanel.add(searchPanel, BorderLayout.NORTH);
 
@@ -80,7 +89,7 @@ public class MainMenu extends JFrame {
             URL imageURL = getClass().getResource("/icon/welcome.jpg");
             if (imageURL != null) {
                 ImageIcon icon = new ImageIcon(ImageIO.read(imageURL));
-                Image image = icon.getImage().getScaledInstance(800, 300, Image.SCALE_SMOOTH); // Điều chỉnh kích thước ảnh
+                Image image = icon.getImage().getScaledInstance(800, 300, Image.SCALE_SMOOTH); 
                 JLabel imageLabel = new JLabel(new ImageIcon(image));
                 topPanel.add(imageLabel, BorderLayout.CENTER);
             } 
@@ -92,19 +101,46 @@ public class MainMenu extends JFrame {
 
         mainPanel.add(topPanel, BorderLayout.NORTH);
 
-        String[] columnNames = {"#", "Name", "Category", "Start Date", "End Date", "Location", "Type"};
+        String[] columnNames = {"Event ID", "Name", "Event Date", "Location", "Type", "Organizer ID", "Description", "Actions"};
         Object[][] data = {
-            {1, "Event 1", "Category 1", "01/01/2024", "01/02/2024", "Location 1", "Type 1"},
-            {2, "Event 2", "Category 2", "02/01/2024", "02/02/2024", "Location 2", "Type 2"}
+            {1, "Class meeting", "01/01/2024", "Ha Noi", "Private", "123", "Description", "Actions1" },
+            {2, "Event 2", "02/01/2024", "Ha Noi", "Public", "456", "Description", "Actions2"}
         }; 
-        JTable eventTable = new JTable(data, columnNames);
+        
+        DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return column == 7;
+            }
+        };
+        JTable eventTable = new JTable(model);
+        JTableHeader header = eventTable.getTableHeader();
+        header.setDefaultRenderer(new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            	JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                label.setFont(label.getFont().deriveFont(Font.BOLD));
+                label.setHorizontalAlignment(SwingConstants.CENTER);
+                label.setBorder(UIManager.getBorder("TableHeader.cellBorder")); 
+                return label;
+            }
+        });
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
         centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
         for (int i = 0; i < eventTable.getColumnCount(); i++) {
             eventTable.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
         }
+        
+        TableColumn actionsColumn = eventTable.getColumnModel().getColumn(7);
+        actionsColumn.setCellRenderer(new ButtonRenderer());
+        actionsColumn.setCellEditor(new ButtonEditor(new JCheckBox()));
+        
+        eventTable.getTableHeader().setReorderingAllowed(false);
+        eventTable.getTableHeader().setResizingAllowed(false);
+
+        
         JScrollPane scrollPane = new JScrollPane(eventTable);
-        scrollPane.setPreferredSize(new Dimension(getWidth(), getHeight() / 3)); // Chỉnh kích thước 1/3 của cửa sổ
+        scrollPane.setPreferredSize(new Dimension(getWidth(), getHeight() / 3));
         mainPanel.add(scrollPane, BorderLayout.SOUTH);
         
         add(leftPanel, BorderLayout.WEST);
@@ -114,7 +150,7 @@ public class MainMenu extends JFrame {
     private JMenu createMenu(String text, Color foreground) {
         JMenu menu = new JMenu(text);
         menu.setForeground(foreground);
-        menu.setBackground(new Color(0, 102, 0)); 
+        menu.setBackground(new Color(255, 223, 128)); 
         menu.setOpaque(true);
         menu.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         return menu;
@@ -123,7 +159,7 @@ public class MainMenu extends JFrame {
     private JMenuItem createMenuItem(String text, Color foreground) {
         JMenuItem menuItem = new JMenuItem(text);
         menuItem.setForeground(foreground);
-        menuItem.setBackground(new Color(0, 102, 0)); 
+        menuItem.setBackground(new Color(255, 223, 128)); 
         return menuItem;
     }
 
